1933575bba4bbd0d2244ca79f18562af0bbac0a1
Emir Turkbey <19turkbey@gmail.com>
2026-04-17 22:18:38 +0300

    feat: update message on server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.status(200).send("Hello world!");
+    res.status(200).send("Hello Aliens!");
 });
 
 app.listen(port, () => {
